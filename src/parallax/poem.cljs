9a388fa1bf5982b0ed869c404a32457354d6de96
zumbalogy <intrapostal@gmail.com>
2016-07-24 18:13:28 -0400

    simplified some helpers and made things auto-lin-break

--- src/parallax/poem.cljs
+++ src/parallax/poem.cljs
@@ -2,6 +2,9 @@
 
 (def br [:br])
 
+(defn paragraph [& args]
+  (map-indexed (fn [i x] [:p {:key i} x]) args))
+
 (defn c [& args]
   (cycle args))
 
@@ -20,33 +23,23 @@
 ; TODO: helper for building thing that types out something letter[/word?] at a time like a typewriter
 ; and also one for deleting them
 
-; TODO: a helper that just goes striaght though and bottoms/cielings out, instead of looking with cycle or mod
-
 (defn build-text [x]
   (let [n (scale x 200)
-        nnth (fn [& args] (nth args (mod n (count args))))
-        xth (fn [s & args] (nth args (mod (scale x s) (count args))))
-        nmod (fn [m & args] (nth args (mod (mod n m) (count args))))
-        btwn (fn [a b & args] (when (<= a n b) (apply nnth args)))
-        ]
-    [:div
-      "this is a "
-      (xth 500 "scrolling" (xth 100 "cool" "c00l") "good" "hard to read")
-      " "
-      (rand-pick 2000 "poem" "message" "story")
-      br
+        nnth (fn [& args] (nth (cycle args) n))
+        xth (fn [s & args] (nnth (scale x s)))
+        nmod (fn [m & args] (nnth (mod n m)))
+        btwn (fn [a b & args] (when (<= a n b) (apply nnth args)))]
+    [:div (paragraph
+      (str "this is a "
+        (xth 500 "scrolling" (xth 100 "cool" "c00l") "good" "hard to read")
+        " "
+        (rand-pick 2000 "poem" "message" "story"))
       (nnth "part" "2" "electirc bugaloo")
-      br
       (nnth [:div "testing p"])
       (nnth "a" "b" (rand-nth ["x" "y"]))
-      br
       (nnth "a" "b" (rand-pick 2000 "x" "y"))
-      br
-      "i am more of a "
-      (get ["morning" "night" "morning" "night" "boring"] n)
-      " person"
-      br
+      (str "i am more of a "
+        (get ["morning" "night" "morning" "night" "boring"] n)
+        " person")
       (btwn 0 50 "this is the start")
-      br
-      (btwn 50 100 "chapter 2" "chapter 2.1")
-    ]))
+      (btwn 50 100 "chapter 2" "chapter 2.1"))]))
